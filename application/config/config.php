215dc01c8ec55d7ff7e329e6d8625ecb97a9df0e
piyo02 <alzidni2000@gmail.com>
2020-05-31 11:55:18 +0800

    a

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/pbw_login/';
+$config['base_url'] = 'https://pbw.akar-z.com/';
+// $config['base_url'] = 'http://localhost/pbw_login/';
 
 /*
 |--------------------------------------------------------------------------
